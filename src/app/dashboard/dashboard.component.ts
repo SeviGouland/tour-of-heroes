f2c11e7e453a03b57150d0a4c729af361fea1654
Sevi Gouland <sevigouland@gmail.com>
2023-09-17 11:17:16 +0300

    add navigation

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,6 +13,7 @@ export class DashboardComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    console.log("EXW KAPSEI EGKEFALIKA KUTTARA");
     this.getHeroes();
   }
 
